bb92546cb98f9dedd1e0970f9358505a63ef28bf
Younggil-kim <gom991@ajou.ac.kr>
2022-03-02 08:55:45 +0900

    Feat: Proxy

--- src/Page/Login.tsx
+++ src/Page/Login.tsx
@@ -63,7 +63,7 @@ function Login() {
     setPassword(event.target.value);
   };
   const dispatch = useDispatch();
-  const LOGIN_URL = "/auth/login";
+  const LOGIN_URL = "http://3.36.69.51/auth/login";
 
   const postLoginData = async () => {
     try {
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,27 +1,27 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(
-    '/myinfo',
-    createProxyMiddleware({
-      target: process.env.REACT_APP_SERVER_URL,
-      changeOrigin: true,
-    })
-  );
-  app.use(
-    '/profile',
-    createProxyMiddleware({
-      target: process.env.REACT_APP_SERVER_URL,
-      changeOrigin: true,
-    })
-  );
-  app.use(
-    '/auth',
-    createProxyMiddleware({
-      target: process.env.REACT_APP_SERVER_URL,
-      changeOrigin: true,
-    })
-  );
+//   app.use(
+//     '/myinfo',
+//     createProxyMiddleware({
+//       target: process.env.REACT_APP_SERVER_URL,
+//       changeOrigin: true,
+//     })
+//   );
+//   app.use(
+//     '/profile',
+//     createProxyMiddleware({
+//       target: process.env.REACT_APP_SERVER_URL,
+//       changeOrigin: true,
+//     })
+//   );
+//   app.use(
+//     '/auth',
+//     createProxyMiddleware({
+//       target: process.env.REACT_APP_SERVER_URL,
+//       changeOrigin: true,
+//     })
+//   );
   app.use(
       '/lol',
       createProxyMiddleware({
